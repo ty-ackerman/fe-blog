963df3e2bda6ab9791112e21bfad46897756d34d
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-10-19 13:43:40 -0400

    sections type dynamically sets based on dropdown selection

--- src/components/BlogEntry.js
+++ src/components/BlogEntry.js
@@ -4,8 +4,14 @@ import BlogEntryRender from "./BlogEntryRender";
 import BlogSectionDropDown from "./BlogSectionDropDown";
 
 export default function BlogEntry() {
-  const SECTION_TYPES = { paragraph: "paragraph", link: "link" };
+  const SECTION_TYPES = {
+    paragraph: "paragraph",
+    link: "link",
+    photo: "photo",
+    blockquote: "blockquote",
+  };
   const [blogObject, setBlogObject] = useState();
+  const [autocompleteValue, setAutocompleteValue] = useState("");
   const paragraphRef = useRef("");
   const titleRef = useRef("");
 
@@ -14,13 +20,20 @@ export default function BlogEntry() {
       title: titleRef.current.value,
       date: new Date().toString(),
       sections: [
-        { type: SECTION_TYPES.paragraph, content: paragraphRef.current.value },
+        {
+          type: SECTION_TYPES[autocompleteValue],
+          content: paragraphRef.current.value,
+        },
       ],
     });
   };
+
   return (
     <div className="flex flex-col items-center">
-      <BlogSectionDropDown SECTION_TYPES={SECTION_TYPES} />
+      <BlogSectionDropDown
+        SECTION_TYPES={SECTION_TYPES}
+        setAutocompleteValue={setAutocompleteValue}
+      />
       <TextField inputRef={titleRef} placeholder="Title"></TextField>
       <TextField inputRef={paragraphRef} placeholder="Your Post"></TextField>
       <Button onClick={sendValue}>Submit</Button>
--- src/components/BlogSectionDropDown.js
+++ src/components/BlogSectionDropDown.js
@@ -1,11 +1,15 @@
-import React from "react";
+import { React } from "react";
 import { Autocomplete, TextField } from "@mui/material";
-export default function BlogSectionDropDown({ SECTION_TYPES }) {
+export default function BlogSectionDropDown({
+  SECTION_TYPES,
+  setAutocompleteValue,
+}) {
   return (
     <div>
       <Autocomplete
         sx={{ width: 300 }}
         options={Object.keys(SECTION_TYPES)}
+        onChange={(event, newValue) => setAutocompleteValue(newValue)}
         renderInput={(params) => <TextField {...params} label="Section Type" />}
       />
     </div>
