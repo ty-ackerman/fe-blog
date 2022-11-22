a69a6c4ca1dc6246aed288d8794064eee4a5b49a
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-11-22 14:33:33 -0500

    article render in switch statement for readability

--- src/components/ArticleRender.js
+++ src/components/ArticleRender.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { isCompositeComponent } from "react-dom/test-utils";
 import { sampledata } from "../sampledata";
 export default function ArticleRender() {
   return (
@@ -7,28 +6,42 @@ export default function ArticleRender() {
       {/* {console.log(sampledata.root.children)} */}
       {sampledata.root.children.map((x) => {
         console.log(x.children);
-        if (x?.tag === "h1") return <h1>{x?.children[0]?.text}</h1>;
-        if (x?.tag === "h2") return <p>{x?.children[0]?.text}</p>;
-        if (x?.tag === "h3") return <p>{x?.children[0]?.text}</p>;
-        // render if there is no tag and children is an array
-        if (!x?.tag)
-          return (
-            <p>
-              {x?.children?.map((x) => {
-                return x?.text;
-              })}
-            </p>
-          );
-        //render for bulleted list
-        if (x?.tag === "ul") {
-          console.log(x?.children[0].children[0].text);
-          return (
-            <p>
-              {x?.children[0].children?.map((x) => {
-                return x?.text;
-              })}
-            </p>
-          );
+
+        switch (x?.tag) {
+          case "h1":
+            return <h1>{x?.children[0]?.text}</h1>;
+          case "h2":
+            return <h2>{x?.children[0]?.text}</h2>;
+          case "h3":
+            return <h3>{x?.children[0]?.text}</h3>;
+          //render for bulleted list
+          case "ul":
+            return (
+              <p>
+                {x?.children[0].children?.map((x) => {
+                  return x?.text;
+                })}
+              </p>
+            );
+          //render for organized list
+          case "ol":
+            return (
+              <p>
+                {x?.children[0].children?.map((x) => {
+                  return x?.text;
+                })}
+              </p>
+            );
+          // render if there is no tag and children is an array
+
+          default:
+            return (
+              <p>
+                {x?.children?.map((x) => {
+                  return x?.text;
+                })}
+              </p>
+            );
         }
       })}
     </div>
